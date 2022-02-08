bdc38a58d57d28d71d600edf3c3144ed4f68ce1f
egbejob680 <jobegbe39@gmail.com>
2022-02-08 21:40:07 +0100

    area of circle

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # day 1:  procedural
-a = int(input("enter any value: "))
+'''a = int(input("enter any value: "))
 b = int(input("enter any value: "))
 c = int(input("enter any value: "))
 total = a + b + c
-print("total is :",total)
+print("total is :",total)'''
+
+
+# calculating area
+pi = 3.14
+r = int(input("enter radius: "))
+area = pi * r**2
+print("the area of a circle is: ",area)
