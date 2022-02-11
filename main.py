dfe298e94d25d7dee0efc9c199daf865827b829b
egbejob680 <jobegbe39@gmail.com>
2022-02-11 20:43:41 +0100

    set operation

--- main.py
+++ main.py
@@ -25,7 +25,15 @@ else:
     print(f"both A and B are equal: {a} == {b}")'''
 
 
-
-num = int(input("displaying the multiplication table of: "))
+#day 3
+'''num = int(input("displaying the multiplication table of: "))
 for i in range(1,11):
-    print(num,'x',i ,'=', num*i)
+    print(num,'x',i ,'=', num*i)'''
+
+    #day4
+    #set operation
+E = {0,3,4,5,6,8};
+N = {1,2,5,7,8,9};
+print("union of E and N is",E|N)
+print('intersection of N and E is' ,E & N)
+print("difference of E and N is" ,E-N)
